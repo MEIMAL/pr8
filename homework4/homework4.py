6cc49093228dac6b39cff7aba374c93688b5ad75
MEIMAL <131466033+MEIMAL@users.noreply.github.com>
2024-10-05 13:44:04 +0500

    Update homework4.py

--- homework4/homework4.py
+++ homework4/homework4.py
@@ -1,1 +1,9 @@
-
+a = int(input("Введите первое число (a): "))
+b = int(input("Введите второе число (b): "))
+# Определение границ
+start = min(a, b) + 1
+end = max(a, b)
+# Вывод целых чисел между a и b
+print(f"Целые числа между {a} и {b}:")
+for i in range(start, end):
+    print(i)
